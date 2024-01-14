8f7c1b7b5a0c477c4c5d281b31b5830030ab9cf6
shahan <shahan.ahmed@northsouth.edu>
2024-01-14 17:47:00 +0600

    title changing

---
+++ 3_Basic_Output.py
@@ -0,0 +1,26 @@
+# inport flet
+
+import flet as ft
+
+'''
+def main(Page:ft.Page):
+    Page.update()
+ft.app(main)
+'''
+
+######################### Title Change #######################
+
+# আমাদের সকল পরিবর্তন হবে Page.update() এর উপরে। 
+# প্রথমেই আসা যাক আমরা চাচ্ছি আমাদের পেইজ এর টাইটেল দিতে। সেটা কীভাবে দিতে হয় 
+# Page.title="This is Title"
+
+# def main(Page:ft.Page):
+#     Page.title="This is Title"
+#     Page.update()
+# ft.app(main)
+
+# তাহলেই শেখা যাবে যে আমাদের পেইজ এর Title Change হয়ে গিয়েছে। মূলত HTML এ এটা হল Header এর টাইটেল এর মতো 
+
+
+###################### Output #########################
+
