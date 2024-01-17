3f6e39cd03d5b23cbae75763d590c15ebf5b7b57
shahan <shahan.ahmed@northsouth.edu>
2024-01-17 17:34:14 +0600

    minor changes to counter

--- 7_counter2.py
+++ 7_counter2.py
@@ -3,11 +3,11 @@ import time
 def main(page: ft.Page):
     page.title="Counter"
 
-    text=ft.Text()
+    text=ft.Text(color="white", size=50)
     page.add(text)
 
     for i in range(11):
-        text.value=i
+        text.value="Counter : "+str(i)
         page.update()
         time.sleep(1)
 ft.app(main)
