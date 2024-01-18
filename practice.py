e3354ad1d24c7c684228c0ed4267532b2bbc04a6
shahan <shahan.ahmed@northsouth.edu>
2024-01-18 20:44:28 +0600

    practice: upto counter version 1

---
+++ practice.py
@@ -0,0 +1,52 @@
+# import flet as ft 
+
+
+# def main(page:ft.Page):
+#     page.title="Practice"
+
+#     textName=ft.Text(value="Shahan Ahmed")
+#     textDept=ft.Text(value="Department of Electrical & computer & Engineering")
+#     textUni=ft.Text(value="North South University, Dhaka, Bangladesh")
+#     textEmail=ft.Text(value="Email: shahan.ahmed@northsouth.edu")
+#     page.controls.append(textName)
+#     page.controls.append(textDept)
+#     page.controls.append(textUni)
+#     page.controls.append(textEmail)
+#     page.update()
+# ft.app(main)
+
+
+### add function isnted of update
+# import flet as ft 
+
+
+# def main(page:ft.Page):
+#     page.title="Practice"
+
+#     textName=ft.Text(value="Shahan Ahmed")
+#     textDept=ft.Text(value="Department of Electrical & computer & Engineering")
+#     textUni=ft.Text(value="North South University, Dhaka, Bangladesh")
+#     textEmail=ft.Text(value="Email: shahan.ahmed@northsouth.edu", color="red")
+#     page.add(textName)
+#     page.add(textDept)
+#     page.add(textUni)
+#     page.add(textEmail)
+# ft.app(main)
+
+
+
+##### Counter version 1
+# import flet as ft 
+# import time 
+
+# def main(page:ft.Page):
+#     page.title="Practice"
+
+#     text=ft.Text(value="COUNTER VERSION 1", color="blue", size=40)
+#     page.add(text)
+
+#     for i in range(11):
+#         text2=ft.Text(value=f"Count: {i}")
+#         page.add(text2)
+#         time.sleep(1)
+# ft.app(main)
