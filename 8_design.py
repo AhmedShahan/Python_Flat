fdf082429df06f4747c9abec91ae32e0dc37689c
shahan <shahan.ahmed@northsouth.edu>
2024-01-18 23:55:18 +0600

    basic design: theme & text color

---
+++ 8_design.py
@@ -0,0 +1,32 @@
+import flet as ft 
+import time 
+from flet import colors
+
+def main(page:ft.Page):
+    page.title="Design in Flet"
+
+    # theme change
+    # page.theme_mode="dark"
+    # page.theme_mode="light"
+
+    # Align all the element into center
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER 
+
+
+
+
+    # text=ft.Text("Design in Flet")
+    # Text Color change
+    # text=ft.Text("Design in Flet", color="red")
+
+    # Change Text size
+    # text=ft.Text("Design in Flet", color="white", size=40)
+    # text=ft.Text("Design in Flet", color=colors.BLUE_700, size=40)
+    text=ft.Text("Design in Flet", color=colors.BLUE_700, size=40, bgcolor="white")
+
+
+
+
+    page.add(text)
+
+ft.app(main)
