6235d053f4e27c5a30b3472ddf467767a8a03cb4
shahan <shahan.ahmed@northsouth.edu>
2024-04-24 00:40:31 +0600

    working 13_todoList

--- 13_todoList.py
+++ 13_todoList.py
@@ -1,31 +1,28 @@
-import flet as ft 
+import flet as ft
+
 
 def main(pg:ft.Page):
-    pg.title="To Do List"
-    pg.theme_mode="dark"
-    pg.window_height="500"
-    pg.window_width="500"
-    pg.horizontal_alignment="CENTER"
+    pg.theme_mode="Dark"
+    pg.title="To do List App"
+    # pg.appbar=ft.AppBar(title=ft.Text("To Do list", size=30),center_title=True)
+    # pg.window_max_height="700"
+    # pg.window_max_width="500"
+    pg.bgcolor="bluegrey900"
+    pg.horizontal_alignment=ft.MainAxisAlignment.CENTER
+    pg.vertical_alignment=ft.MainAxisAlignment.CENTER
 
-    def buttonClick(e):
-        text=ft.Text(value=textInput.value)
-        pg.add(text)
-        # text=textInput.value
-        print(text)
+    pg.add(ft.TextField("Shahan Ahmed"))
 
+    ## Main Container
+    # pg.add(
+    #     ft.Container(
+    #         width="300",
+    #         height="500",
+    #         bgcolor="black",
+    #         # alignment=ft.MainAxisAlignment.CENTER
+    #         # alignment=ft.MainAxisAlignment.CENTER
+    #     )
+    # )
+    pg.update()
 
-    pg.add(ft.Text("TO DO LIST",size=40, color="BLUE"))
-
-    textInput=ft.TextField(border_color="white")
-    # pg.add(textInput)
-
-    addButton=ft.ElevatedButton(text="ADD", on_click=buttonClick)
-    # pg.add(addButton)
-
-    pg.add(ft.Row(controls=[textInput,addButton],alignment=ft.MainAxisAlignment.SPACE_BETWEEN))
-
-    pg.add(ft.Text("\nYour Listed Items Are", color="Green",size=30))
-    
-
-
-ft.app(target=main)
+ft.app(target=main, view=ft.WEB_BROWSER)
